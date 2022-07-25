c5d4d71352d31f6248607b5559ea0880158eb354
Ahmedounet <ahmed.zouaoui.555@gmail.com>
2022-07-25 09:24:23 +0200

    pr fixes

--- core/iwasm/libraries/wasi-nn/test/run.sh
+++ core/iwasm/libraries/wasi-nn/test/run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-/home/ahmedounet/Desktop/wasi_nn_tflite/wasm-micro-runtime/product-mini/platforms/linux/build/iwasm --dir=. testapp.wasm
+$PWD/../../../../../product-mini/platforms/linux/build/iwasm --dir=. testapp.wasm
