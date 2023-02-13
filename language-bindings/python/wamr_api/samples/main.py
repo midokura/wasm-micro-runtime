c3096fc576ac32f6ed31ebbc1a5228619d75da62
tonibofarull <toni.bofarull@midokura.com>
2023-02-13 20:18:33 +0100

    Add license

--- language-bindings/python/wamr_api/samples/main.py
+++ language-bindings/python/wamr_api/samples/main.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2019 Intel Corporation.  All rights reserved.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 from wamr.wamr_api.wamr import Engine, Module, Instance, ExecEnv
 from ctypes import c_uint
 import pathlib
