e5b52344fdc41392e13c1f0eb24ff79b3d35924b
tonibofarull <toni.bofarull@midokura.com>
2023-02-14 13:03:54 +0100

    Pre-install script for linux

---
+++ language-bindings/python/utils/create_lib.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+CUR_DIR=$(cd $(dirname $0) && pwd -P)
+ROOT_DIR=${CUR_DIR}/../../..
+
+WAMR_BUILD_PLATFORM=${WAMR_BUILD_PLATFORM:-"linux"}
+
+cd ${ROOT_DIR}/product-mini/platforms/${WAMR_BUILD_PLATFORM}
+
+mkdir -p build && cd build
+cmake ..
+make -j
+
+cp *libiwasm* ${CUR_DIR}/../src/wamr/libs
