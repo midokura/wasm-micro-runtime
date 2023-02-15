193321af8d1f3bebe7fa64172e4a85ecb51a0083
YAMAMOTO Takashi <yamamoto@midokura.com>
2023-02-15 16:24:04 +0900

    wip

--- language-bindings/python/src/wamr/wamr_api/wamr.py
+++ language-bindings/python/src/wamr/wamr_api/wamr.py
@@ -17,16 +17,19 @@ from wamr.wamr_api.iwasm import String
 
 from wamr.wamr_api.iwasm import Alloc_With_Pool
 from wamr.wamr_api.iwasm import RuntimeInitArgs
+from wamr.wamr_api.iwasm import wasm_application_execute_main
 from wamr.wamr_api.iwasm import wasm_exec_env_t
 from wamr.wamr_api.iwasm import wasm_function_inst_t
 from wamr.wamr_api.iwasm import wasm_module_inst_t
 from wamr.wamr_api.iwasm import wasm_module_t
+from wamr.wamr_api.iwasm import wasm_runtime_call_indirect  # XXX
 from wamr.wamr_api.iwasm import wasm_runtime_call_wasm
 from wamr.wamr_api.iwasm import wasm_runtime_create_exec_env
 from wamr.wamr_api.iwasm import wasm_runtime_deinstantiate
 from wamr.wamr_api.iwasm import wasm_runtime_destroy
 from wamr.wamr_api.iwasm import wasm_runtime_destroy_exec_env
 from wamr.wamr_api.iwasm import wasm_runtime_full_init
+from wamr.wamr_api.iwasm import wasm_runtime_get_module_inst
 from wamr.wamr_api.iwasm import wasm_runtime_instantiate
 from wamr.wamr_api.iwasm import wasm_runtime_load
 from wamr.wamr_api.iwasm import wasm_runtime_lookup_function
@@ -89,12 +92,23 @@ class Module:
 
 class Instance:
     def __init__(self, module: Module):
-        self.module = module
-        self.module_inst = self._create_module_inst(module)
+        if module is not None:
+            self.module = module
+            self.module_inst = self._create_module_inst(module)
+            self.own_c = True
+
+    @classmethod
+    def wrap(cls, c_inst: wasm_module_inst_t):
+        i = cls(None)
+        i.module = None
+        i.module_inst = cast(c_inst, wasm_module_inst_t)
+        i.own_c = False
+        return i
 
     def __del__(self):
-        print("deleting Instance")
-        wasm_runtime_deinstantiate(self.module_inst)
+        if self.own_c:
+            print("deleting Instance")
+            wasm_runtime_deinstantiate(self.module_inst)
 
     def malloc(self, nbytes: int, native_handler) -> c_uint:
         return wasm_runtime_module_malloc(self.module_inst, nbytes, native_handler)
@@ -114,18 +128,43 @@ class Instance:
             raise Exception("Error while creating module instance")
         return module_inst
 
+    def application_execute_main(self, args) -> bool:
+        # TODO convert args
+        c_ret = wasm_application_execute_main(self.module_inst, 0, None)
+        return bool(c_ret)
+
 
 class ExecEnv:
     def __init__(self, module_inst: Instance):
-        self.module_inst = module_inst
-        self.exec_env = self._create_exec_env(module_inst)
+        if module_inst is not None:
+            self.exec_env = self._create_exec_env(module_inst)
+            self.module_inst = module_ist
+            self.own_c = True
+
+    @classmethod
+    def wrap(cls, c_env: wasm_exec_env_t):
+        i = cls(None)
+        i.exec_env = cast(c_env, wasm_exec_env_t)
+        i.module_inst = None
+        i.own_c = False
+        return i
+
+    def get_module_inst(self):
+        if self.module_inst is not None:
+            return self.module_inst
+        return Instance.wrap(wasm_runtime_get_module_inst(self.exec_env))
 
     def __del__(self):
-        print("deleting ExecEnv")
-        wasm_runtime_destroy_exec_env(self.exec_env)
+        if self.own_c:
+            print("deleting ExecEnv")
+            wasm_runtime_destroy_exec_env(self.exec_env)
 
     def call(self, func: wasm_function_inst_t, argc: int, argv: "POINTER[c_uint]"):
         if not wasm_runtime_call_wasm(self.exec_env, func, argc, argv):
+            raise Exception("Error while calling function")
+
+    def call_indirect(self, elem_idx: int, argc: int, argv: "POINTER[c_uint]"):
+        if not wasm_runtime_call_indirect(self.exec_env, elem_idx, argc, argv):
             raise Exception("Error while calling function")
 
     def _create_exec_env(self, module_inst: Instance) -> wasm_exec_env_t:
