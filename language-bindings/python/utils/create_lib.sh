c4aa26a4ac61757b3dc081727d4a46b247815197
aoyamaayako <aoyamaayako@gmail.com>
2023-04-17 11:59:08 +0200

    Remove unnessesary space

--- language-bindings/python/utils/create_lib.sh
+++ language-bindings/python/utils/create_lib.sh
@@ -15,7 +15,7 @@ mkdir -p build && cd build
 cmake -DWAMR_BUILD_DEBUG_INTERP=1 \
       -DWAMR_BUILD_LIB_PTHREAD=1 \
       -DWAMR_BUILD_LIB_WASI_THREADS=1 \
-      -DWAMR_BUILD_LIB_WASI=1 \      
+      -DWAMR_BUILD_LIB_WASI=1 \
 ..
 make -j
 
